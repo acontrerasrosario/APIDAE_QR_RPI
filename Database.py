1f0cd48089c5b3e1568920bd5b0b56ea78793493
Angel Contreras <acontrerasrosario@gmail.com>
2017-03-25 23:18:11 -0400

    creating functions to control information about database

--- Database.py
+++ Database.py
@@ -1,16 +1,30 @@
 import pyrebase
-
+import RegisterTime as time
 config = {
-      "apiKey": "AIzaSyB0IXn8A10yGVM5rGR871L7i2BapIOMgQQ",
-      "authDomain": "apidae-4f97d.firebaseapp.com",
-      "databaseURL": "https://apidae-4f97d.firebaseio.com",
-      "storageBucket": "apidae-4f97d.appspot.com"
+      "apiKey": "AIzaSyAVJ5yBzKo2wDpm6fPtAPLmMEydVTz0GTk",
+      "authDomain": "apidae-85302.firebaseapp.com",
+      "databaseURL": "https://apidae-85302.firebaseio.com",
+      "storageBucket": "apidae-85302.appspot.com"
     }
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
-user = auth.sign_in_with_email_and_password('acontrerasrosario@gmail.com','emilio123456')
+user = auth.sign_in_with_email_and_password('acontrerasrosario@gmail.com', '123456')
 db = firebase.database()
 
+# a list of sections available
+sectionsList = []
 
-def startconfig():
+# check if is the ID is a professor
+def isProfessor(id):
+    result = db.child('Persons').child(id).child('type').get().val()
+    if(result != 1):
+        return True
+    else:
+        return False
+
+# return all sections available in a list
+def sections():
+    result = db.child('Section').order_by_key().get()
+    for x in result.val():
+        sectionsList.append(x)
 
