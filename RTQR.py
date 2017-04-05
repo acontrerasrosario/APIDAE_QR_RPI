1b4a4167cd12735f761f394ff461220f494f9862
Angel Contreras <acontrerasrosario@gmail.com>
2017-04-05 19:13:27 +0000

    implementing RTQR in RASPBERRY PI

--- RTQR.py
+++ RTQR.py
@@ -1,53 +1,53 @@
-import zbar
-import datetime
-from PIL import Image
+
+# import the necessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
 import cv2
-import RegisterTime as time
+from PIL import Image
+import zbar
 
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
+ 
+# allow the camera to warmup
+time.sleep(0.1)
+ 
+# capture frames from the camera
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+	# grab the raw NumPy array representing the image, then initialize the timestamp
+	# and occupied/unoccupied text
+	image = frame.array
+ 
+	# show the frame
+	cv2.imshow("Frame", image)
+	key = cv2.waitKey(1) & 0xFF
+ 
+	# clear the stream in preparation for the next frame
+	rawCapture.truncate(0)
 
-
-
-
-
-
-def main():
-
-
-
-    capture = cv2.VideoCapture(0)
-
-    while True:
-        # To quit this program press q.
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-        # Breaks down the video into frames
-        ret, frame = capture.read()
-
-        # Displays the current frame
-        cv2.imshow('Current', frame)
-
-        # Converts image to grayscale.
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	# Converts image to grayscale.
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
         # Uses PIL to convert the grayscale image into a ndary array that ZBar can understand.
         image = Image.fromarray(gray)
         width, height = image.size
         zbar_image = zbar.Image(width, height, 'Y800', image.tobytes())
 
-        # Scans the zbar image.
+        
+ 
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
+
+
+	# Scans the zbar image.
         scanner = zbar.ImageScanner()
         scanner.scan(zbar_image)
 
         # Prints data from image.
         for decoded in zbar_image:
-            data = {
-                "llegada":time.currentTime()
-            }
-            db.child('Records').child('S1').child(time.currentDate()).child('student').child(decoded.data).set(data)
-
             print(decoded.data)
-
-
-if __name__ == "__main__":
-    main()
